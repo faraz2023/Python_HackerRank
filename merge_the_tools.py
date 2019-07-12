2ea74f64ad0e512e81ebf5563607e17c363e466c
faraz khoshbakhtian <39635965+faraz2023@users.noreply.github.com>
2019-07-12 19:50:53 -0400

    Create merge_the_tools.py

---
+++ merge_the_tools.py
@@ -0,0 +1,13 @@
+#medium difficulty
+
+
+def merge_the_tools(string, k):
+    for part in zip(*[iter(string)] * k):
+        d = dict()
+        print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
+
+# your code goes here
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
